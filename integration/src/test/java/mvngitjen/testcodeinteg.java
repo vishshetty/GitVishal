5fa81ad6eb9ca8dbb26fa82e689ff117d6d6cd60
Vishal <Vishal@DESKTOP-VR4FGIR>
2019-06-21 13:27:57 +0530

    new project created to test the git jenkings integration on mvn project..

---
+++ integration/src/test/java/mvngitjen/testcodeinteg.java
@@ -0,0 +1,30 @@
+package mvngitjen;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class testcodeinteg {
+	
+	public static void main(String[] args) {
+		String s="hii";
+		
+		char[] charArray = s.toCharArray();
+		HashMap<Character,Integer> HS=new HashMap<Character,Integer>();
+		for (char c : charArray) {
+			if (HS.containsKey(c)) {
+				HS.put(c, HS.get(c)+1);
+			}else{
+				HS.put(c, 1);	
+				}
+			}
+		
+		Set<Entry<Character, Integer>> entrySet = HS.entrySet();
+        for (Entry<Character, Integer> entry : entrySet) {
+			System.out.println(entry);
+		}
+		
+		
+	}
+
+}
